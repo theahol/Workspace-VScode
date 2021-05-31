6b6ef4fa42bff9fae26655896b9aa20f397de48d
theahol <71002654+theahol@users.noreply.github.com>
2021-05-31 16:42:13 +0200

    Add files via upload

---
+++ fromSimtoRobFirstTry.py
@@ -0,0 +1,78 @@
+import sys
+import csv
+import socket
+import time
+from moto.simple_message import *
+from moto import motion_connection
+from moto.simple_message_connection import SimpleMessageConnection
+from moto.simple_message import SimpleMessage
+from moto import Moto
+from moto import Moto, ControlGroupDefinition
+from moto.simple_message import (
+    JointFeedbackEx,
+    JointTrajPtExData,
+    JointTrajPtFull,
+    JointTrajPtFullEx,  
+)
+import copy
+from opcua import ua, Server
+import numpy as np
+
+def disconnect(self):
+    client.close()
+
+if __name__ == "__main__":
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # setup our server
+    server = Server()
+    server.set_endpoint("opc.tcp://localhost:4840/motopcua/server/")
+
+    # setup our own namespace, not really necessary but should as spec
+    uri = "http://server.motopcua.github.io"
+    idx = server.register_namespace(uri)
+
+    # get Objects node, this is where we should put our nodes
+    objects = server.get_objects_node()
+
+    # populating our address space
+    myobj = server.nodes.objects.add_object(idx, "Moto")
+    myvar = myobj.add_variable(idx, "MyVariable", 6.7)
+
+    myvar.set_writable()    # Set MyVariable to be writable by clients
+    
+    # Adding desired variables
+    s = myobj.add_variable(idx, "s", 6.7)
+    l = myobj.add_variable(idx, "l", 6.7)
+    u = myobj.add_variable(idx, "u", 6.7)
+    r = myobj.add_variable(idx, "r", 6.7)
+    b = myobj.add_variable(idx, "b", 6.7)
+    t = myobj.add_variable(idx, "t", 6.7)
+
+    s.set_writable()
+    l.set_writable()
+    u.set_writable()
+    r.set_writable()
+    b.set_writable()
+    t.set_writable()
+
+    # starting!
+    server.start()
+    print("server is started")
+    try:
+
+        while True:
+
+            time.sleep(0.1)
+            new_s = s.get_value()
+            print(new_s)
+            new_l = l.get_value()
+            new_u = u.get_value()
+            new_r = r.get_value()
+            new_b = b.get_value()
+            new_t = t.get_value()
+    
+    finally:
+        #close connection, remove subcsriptions, etc
+        server.stop()
